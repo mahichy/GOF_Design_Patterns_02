1ba4f5cfdce01487872acd27793f2077e132aeac
mahi <mohiddinoshim@yahoo.com>
2020-12-20 19:05:14 +0600

    Decorator pattern test done

--- Decorator/lib/product.rb
+++ Decorator/lib/product.rb
@@ -1,24 +1,40 @@
+require "forwardable"
+
 class Product
+	attr_reader :name, :category, :tags
 	def initialize name, category, tags = []
 		@name = name 
 		@category = category
 		@tags = tags
 	end
 
+end
+
+class ProductDecorator
+	extend Forwardable 
+
+	def_delegators :@context, :name, :category, :tags
+
+	def initialize(context)
+		@context = context
+	end
 	def as_html_row
-		 <<EOF
+		     <<EOF
 		<tr> 
-			<th>Tomato</th>
-			<td>fruit </td>
-			<td>
-				<ul>
-					<li>red</li>
-					<li>salad</li>
-					<li>sour</li>
-					<li></li>
-				</ul>
-			</td>
+		<th>#{name}</th>
+		<td>#{category.to_s}</td>
+		<td>
+			#{tags_list}
+		</td>
 		</tr>
+EOF
+	end
+
+	def tags_list
+		<<EOF
+		<ul>
+			#{tags.map { |tag| "<li>#{tag}</li>" }.join("")}
+		</ul>
 EOF
 	end
 end
--- Decorator/test/decorator_pattern_test.rb
+++ Decorator/test/decorator_pattern_test.rb
@@ -2,24 +2,26 @@ require "minitest/autorun"
 require_relative "../lib/product"
 
 describe Product do
-	let(:product) { Product.new("Tomato", :fruit, [ %w(red salad sour) ]) }
+	let(:product) { Product.new("Tomato", :fruit, %w(red salad sour)) }
 	let(:expected_result) { <<EOF
 		<tr> 
-			<th>Tomato</th>
-			<td>fruit </td>
-			<td>
-				<ul>
-					<li>red</li>
-					<li>salad</li>
-					<li>sour</li>
-					<li></li>
-				</ul>
-			</td>
+		<th>Tomato</th>
+		<td>fruit</td>
+		<td>
+			<ul>
+				<li>red</li>
+				<li>salad</li>
+				<li>sour</li>
+			</ul>
+		</td>
 		</tr>
 EOF
-}
-
+	}
 	it "can be printed as a HTML table row" do
-		product.as_html_row.must_equal expected_result
+		product_decorator = ProductDecorator.new product
+		product_decorator.as_html_row.
+		strip.gsub(/(^\s+)|\n/, ""). # remove all indentation and newlines
+		must_equal expected_result.
+		strip.gsub /(^\s+)|\n/, ""   # remove all indentation and newlines
 	end
 end
