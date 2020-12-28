2b4b811be3e2ad28d2440760f0debfa7ae6d9224
mahi <mohiddinoshim@yahoo.com>
2020-12-28 23:08:09 +0600

    Proxy test one pass

---
+++ Proxy/lib/proxy.rb
@@ -0,0 +1,3 @@
+class VideoAuthenticationProxy
+
+end
---
+++ Proxy/test/proxy_test.rb
@@ -0,0 +1,11 @@
+require "minitest/autorun"
+require_relative "../lib/proxy"
+require_relative "../lib/video"
+
+describe VideoAuthenticationProxy do 
+	it "allows priviliged users to access the video" do
+	end
+
+	it "forbids regular users to access the videos" do
+	end
+end
